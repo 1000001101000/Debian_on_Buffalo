d44303833ca0a0bbf778c9681be38a9c05531a18
Jeremy J. Peper <jeremy@earth>
2019-01-05 17:08:10 -0600

    first commit of updated installer which removes teh need for most of the manual steps also adds support for settting MAC addresses when multiple NICs present (ts3400d)

--- Stretch/installer_images/build/payload/source/get_mac.sh
+++ Stretch/installer_images/build/payload/source/get_mac.sh
@@ -10,6 +10,10 @@ modprobe m25p80
 sleep 2
 modprobe mtdblock
 sleep 2
-/source/fw_printenv -n eth1addr > /source/eth0-mac.txt && /source/set_mac.sh "eth0" "$(cat /source/eth0-mac.txt)"
-/source/fw_printenv -n eth2addr > /source/eth1-mac.txt && /source/set_mac.sh "eth1" "$(cat /source/eth1-mac.txt)"
+if [ $? -eq 0 ]; then
+   /source/fw_printenv -n ethaddr > /source/eth0-mac.txt && /source/set_mac.sh "eth0" "$(cat /source/eth0-mac.txt)"
+   /source/fw_printenv -n eth1addr > /source/eth1-mac.txt && /source/set_mac.sh "eth1" "$(cat /source/eth1-mac.txt)"
+else
+   /source/fw_printenv -n eth1addr > /source/eth0-mac.txt && /source/set_mac.sh "eth0" "$(cat /source/eth0-mac.txt)"
+fi
 exit 0
