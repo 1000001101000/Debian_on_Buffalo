0e8448b77910bb82bee8c6549771dba70467d443
Jeremy J. Peper <jeremy@earth>
2019-01-06 11:15:27 -0600

    remove a test script, update installer image with new db file

--- Stretch/installer_images/build/payload/source/update.sh
+++
@@ -1,3 +0,0 @@
-mount /dev/sda2 /mnt                                                                                         
-mount /dev/sda1 /mnt/boot                                           
-chroot /mnt /bin/bash -c "/usr/bin/scp jeremy@earth:~/repo/Debian_on_Buffalo/Stretch/installer_images/build/*.buffalo /boot/"
