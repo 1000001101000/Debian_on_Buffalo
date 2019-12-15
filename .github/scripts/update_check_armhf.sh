397b1a415469337e6a2af83214a29fd2a439e8bf
1000001101000 <38921757+1000001101000@users.noreply.github.com>
2019-12-14 22:26:21 -0600

    Update update_check_armhf.sh

--- .github/scripts/update_check_armhf.sh
+++ .github/scripts/update_check_armhf.sh
@@ -16,10 +16,11 @@ do
   fi
   if [ $? -eq 0 ]; then
     echo "copy to proper dir"
-    ls output/
-    #cp "/tmp/latest.txt" "last_build.txt"
-    #git commit -a -m "generate images based on latest debian installer" 
-    #echo "::set-output name=commit_needed::yes"
+    cp -v output/initrd.buffalo.armhf ../armhf_devices/
+    cp -v output/uImage.buffalo.* ../armhf_devices/
+    cp "/tmp/latest.txt" "last_build.txt"
+    git commit -a -m "generate images based on latest debian installer" 
+    echo "::set-output name=commit_needed::yes"
   fi
   cd "$svpwd"
 done
