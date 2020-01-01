7e0f6941631311a69d81ea2174a7745b4288aa3a
Jeremy J. Peper <jeremy@earth>
2020-01-01 16:52:58 -0600

    add diff based config

--- Tools/kernel_tools/update_kernel_source.sh
+++ Tools/kernel_tools/update_kernel_source.sh
@@ -6,10 +6,11 @@ kernel_ver="$1"
 apt-get update
 apt-get install linux-source-$kernel_ver
 
-rm -r linux-source-$kernel_ver/
-rm -r vanilla-source-$kernel_ver/
+rm -r linux-source-$kernel_ver/ 2> /dev/null
 tar xf /usr/src/linux-source-$kernel_ver.tar.xz
-cp -r linux-source-$kernel_ver/ vanilla-source-$kernel_ver
+if [ "$?" -ne 0 ]; then
+   exit 1
+fi
 
 user="$(ls -la . | grep -e ^d | head -n 1 | gawk '{print $3}')"
 group="$(ls -la . | grep -e ^d | head -n 1 | gawk '{print $4}')"
@@ -18,8 +19,10 @@ cd linux-source-$kernel_ver
 for patch in $(ls ../patches/$kernel_ver)
 do
   patch -p1 < ../patches/$kernel_ver/$patch
+  if [ "$?" -ne 0 ]; then
+   exit 1
+  fi
 done
 cd ..
 
 chown -R $user:$group linux-source-$kernel_ver
-chown -R $user:$group vanilla-source-$kernel_ver
