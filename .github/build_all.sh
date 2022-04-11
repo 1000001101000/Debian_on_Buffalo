c9b911a13fc3d9a3fc4869ed9cf9c9d9ae70e45a
1000001101000 <1000001101000>
2022-04-11 10:15:22 -0500

    add script to make builds easier

---
+++ .github/build_all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+scripts="$(find . | grep generate_images | xargs -n 1 dirname | sort -u)"
+archs="armel armhf"
+oldpwd="$PWD"
+
+for x in $scripts
+do
+  cd "$x"
+#  echo $PWD; exit
+  for arch in $archs
+  do
+    rm ${arch}-files/* 2>/dev/null
+    ./generate_images_${arch}.sh
+    mkdir ../${arch}_devices 2>/dev/null
+    cp -v output/initrd.buffalo.$arch ../${arch}_devices/initrd.buffalo
+    cp -v output/uImage*buffalo.* ../${arch}_devices/
+    cat ${arch}-files/initrd.gz | md5sum > last_build_${arch}.txt
+  done
+  cd "$oldpwd"
+done
